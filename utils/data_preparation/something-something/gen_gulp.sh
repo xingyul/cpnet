4af1c2b1618d2c336768aaeca2b71195884d7499
Xingyu Liu <liuxy610042@gmail.com>
2019-07-23 16:44:45 -0700

    changed to default image size

--- utils/data_preparation/something-something/gen_gulp.sh
+++ utils/data_preparation/something-something/gen_gulp.sh
@@ -6,26 +6,26 @@ export PYTHONPATH=$PYTHONPATH:$1
 python3 -m gulpio.GulpAdapter \
     --json_file /datasets/something-something/val_labels.json \
     --input_path /datasets/something-something/mp4_cat/val \
-    --output_path /datasets/something-something/gulp_256/val \
+    --output_path /datasets/something-something/gulp_240/val \
     --num_workers 4 \
-    --frame_size 256 \
+    --frame_size 240 \
     --frame_rate 24 \
     --skip_exist
 
 python3 -m gulpio.GulpAdapter \
     --json_file /datasets/something-something/train_labels.json \
     --input_path /datasets/something-something/mp4_cat/train \
-    --output_path /datasets/something-something/gulp_256/train \
+    --output_path /datasets/something-something/gulp_240/train \
     --num_workers 4 \
-    --frame_size 256 \
+    --frame_size 240 \
     --frame_rate 24 \
     --skip_exist
 
 python3 -m gulpio.GulpAdapter \
     --json_file /datasets/something-something/test_labels.json \
     --input_path /datasets/something-something/mp4_cat/test \
-    --output_path /datasets/something-something/gulp_256/test \
+    --output_path /datasets/something-something/gulp_240/test \
     --num_workers 4 \
-    --frame_size 256 \
+    --frame_size 240 \
     --frame_rate 24 \
     --skip_exist
